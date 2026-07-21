e3f32d60234d623e15f04c488530bec960b09faf
Griss <grissrll@gmail.com>
2026-07-21 16:17:28 +0300

    feat: add dependency for repositories
    - get_operation_repository
    - get_receipt_repository
    - get_event_repository
    - get_dispatch_repository

--- app/api/dependency.py
+++ app/api/dependency.py
@@ -1,6 +1,11 @@
-from fastapi import Request
+from fastapi import Request, Depends
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.repositories.operations import OperationRepository
+from app.repositories.receipt import ReceiptRepository
+from app.repositories.operation_event import OperationEventRepository
+from app.repositories.dispatch import PaymentDispatchRepository
 
 
 async def get_session(request: Request) -> AsyncGenerator[AsyncSession, None]:
@@ -8,3 +13,27 @@ async def get_session(request: Request) -> AsyncGenerator[AsyncSession, None]:
 
     async with session_maker() as session:
         yield session
+
+
+async def get_operation_repository(
+    db: AsyncSession = Depends(get_session),
+) -> OperationRepository:
+    return OperationRepository(db=db)
+
+
+async def get_receipt_repository(
+    db: AsyncSession = Depends(get_session),
+) -> ReceiptRepository:
+    return ReceiptRepository(db=db)
+
+
+async def get_event_repository(
+    db: AsyncSession = Depends(get_session),
+) -> OperationEventRepository:
+    return OperationEventRepository(db=db)
+
+
+async def get_dispatch_repository(
+    db: AsyncSession = Depends(get_session),
+) -> PaymentDispatchRepository:
+    return PaymentDispatchRepository(db=db)
